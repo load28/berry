68d5cd21876d3f7e2c1b686dec422fa025d46217
load28 <tjalsdud89@naver.com>
2023-03-01 22:09:48 +0900

    apply module federation

--- packages/client/webpack.prod.js
+++ packages/client/webpack.prod.js
@@ -9,7 +9,7 @@ module.exports = merge(common, {
   devtool: "cheap-module-source-map",
   output: {
     filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "../dist"),
+    path: path.resolve(__dirname, "dist"),
     publicPath: "./",
     clean: true,
   },
