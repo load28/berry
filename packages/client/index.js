e350813b7e7e72606e315623c4d55d5d505eb66e
load28 <tjalsdud89@naver.com>
2023-03-01 11:29:22 +0900

    init

---
+++ packages/client/index.js
@@ -0,0 +1,4 @@
+import {hello, test} from 'ui';
+
+hello();
+test();
---
+++ packages/ui/index.js
@@ -0,0 +1,8 @@
+export const hello = () => {
+  console.log("리얼로 변경됨");
+};
+
+
+export const test = () => {
+  console.log('test');
+}
