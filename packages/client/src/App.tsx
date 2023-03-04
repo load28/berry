54b7375eecf17c23a4a812cba8aabd4090cfe279
load28 <tjalsdud89@naver.com>
2023-03-04 12:24:12 +0900

    add route

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,14 +1,21 @@
-import React, {Suspense} from "react";
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import styles from './app.module.scss';
+import RedirectionPage from "./pages/redirection/RedirectionPage";
 
+// lazy loading component
 const LoginPage = React.lazy(() => import("ui/LoginPage"));
 
 const Root: React.FC = () => {
   return (
     <div className={styles.page}>
-      <Suspense>
-        <LoginPage/>
-      </Suspense>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RedirectionPage/>}/>
+          <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/feed" element={<LoginPage/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
---
+++ packages/client/src/pages/redirection/RedirectionPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RedirectionPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Redirection Page</h1>
+    </div>
+  );
+};
+
+export default RedirectionPage;
