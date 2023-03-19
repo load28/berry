0c78f3d404ac0b031e78b5ffd833b62ea55c3e1c
load28 <tjalsdud89@naver.com>
2023-03-19 21:32:46 +0900

    batch test

--- packages/client/src/pages/SignalTest.tsx
+++ packages/client/src/pages/SignalTest.tsx
@@ -1,14 +1,26 @@
 import React from "react";
-import {computed, signal} from "@preact/signals-react";
+import {batch, computed, effect, signal,} from "@preact/signals-react";
 
 
 const SignalTest = () => {
   const count = signal(0);
   const double = computed(() => count.value * 2);
 
+  effect(() => {
+    console.log('count changed', count.value);
+  });
+
+  effect(() => {
+    console.log('double changed', double.value);
+  });
+
   return (
     <div>
-      <button onClick={() => count.value++}>
+      <button onClick={() => batch(() => {
+        count.value++;
+        count.value++;
+        count.value++;
+      })}>
         <>click: {count}</>
       </button>
       <div>
